4ac796dad1cac56f0b56bd8ef02f3e77aed79eb2
Deepak Kumar Kar <kar.deepak2011@gmail.com>
2020-05-16 19:37:26 +0530

    CF

---
+++ competitive/Codeforces/1355A.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <climits>
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define srep(i, begin, end) for (__typeof(end) i = begin; i != end; i++)
+#define si(x) int x = scanInt();
+#define sll(x) LL x = scanLong();
+#define sci(x) int x; scanf("%d", &x);
+#define scll(x) LL x; scanf("%lld", &x);
+#define pi(x) printf("%d ", x)
+#define pll(x) printf("%lld ", x)
+#define nl printf("\n")
+#define clr(a) memset(a, 0, sizeof(a))
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef unsigned int UI; // 32 bit integer
+typedef long int LI; // 32 bit integer
+typedef unsigned long int ULI; // 32 bit unsigned integer
+typedef long long int LL; // 64 bit integer
+typedef unsigned long long int ULL; // 64 bit unsigned integer
+typedef long double LD;
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef deque<int> DI;
+typedef deque<LL> DLL;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+const LL MOD = 1e9+7;
+
+/* Fast I/O */
+inline int scanInt() {
+	int n = 0;
+	char ch = getchar();
+	int sign = 1;
+	while(ch < '0' || ch > '9') {
+		if(ch == '-')	sign = -1;
+		ch = getchar();
+	}
+	while(ch >= '0' && ch <= '9') {
+		n = (n<<1)+(n<<3)+(int)(ch-'0');
+		ch = getchar();
+	}
+	return n*sign;
+}
+
+inline LL scanLong() {
+	LL n = 0;
+	char ch = getchar();
+	LL sign = 1;
+	while(ch < '0' || ch > '9') {
+		if(ch == '-')	sign = -1;
+		ch = getchar();
+	}
+	while(ch >= '0' && ch <= '9') {
+		n = (n<<1)+(n<<3)+(LL)(ch-'0');
+		ch = getchar();
+	}
+	return n*sign;
+}
+
+PLL min_max_digits(LL n) {
+	PLL min_max = make_pair(n%10, n%10);
+	while(n != 0) {
+		LL d = n % 10;
+		min_max.first = min(min_max.first, d);
+		min_max.second = max(min_max.second, d);
+		n = n/10;
+	}
+	return min_max;
+}
+
+LL solve(LL a, LL k) {
+	rep(i, 2, k+1) {
+		PLL min_max = min_max_digits(a);
+		a = a + min_max.first * min_max.second;
+		if(min_max.first * min_max.second == 0)	break;
+	}
+	return a;
+}
+
+int main() {
+	sll(t);
+	rep(_, 0, t) {
+		sll(a); sll(k);
+		pll(solve(a, k)); nl;
+	}
+}
